3e4d2d97b6e52b49326c46230215d2d70534273d
ShaniGamage <shaninehansa716@gmail.com>
2024-12-03 19:28:25 +0530

    create cart

--- food_delivery/src/components/Navbar/Navbar.jsx
+++ food_delivery/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setShowLogin}) => {
   
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt='logo' className='logo'></img>
+        <Link to='/'><img src={assets.logo} alt='logo' className='logo'></img></Link>
         <ul className='navbar-menu'>
             <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"? "active":" "}>Home</Link>
             <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu"? "active":" "}>Menu</a>
@@ -20,7 +20,7 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-right'>
             <img src={assets.search_icon} alt='search-icon'></img>
             <div className='navbar-search-icon'>
-                <img src={assets.basket_icon} alt=''></img>
+                <Link to='/cart'><img src={assets.basket_icon} alt=''></img></Link>
                 <div className='dot'></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>sign in</button>
